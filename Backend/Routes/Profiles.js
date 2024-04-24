e99cc8f5d0cf7268ab47f20ef9ba89118cbda0c2
Abhishek Tiwari <137699878+Absk-tiwari@users.noreply.github.com>
2024-04-24 11:02:25 +0530

    Update Profiles.js

--- Backend/Routes/Profiles.js
+++ Backend/Routes/Profiles.js
@@ -23,7 +23,7 @@ router.post('/update',[ upload.single('image'), fetchuser], async(req,res)=>{
 		}
 		if(req.file) {
 			const uploaded = await uploadOnCloudnary(req.file.path)
-			updateSet.profile:uploaded.url
+			updateSet.profile=uploaded.url
 		}
 	        updateSet.private = req.body.private?req.body.private:false
         const update = {
